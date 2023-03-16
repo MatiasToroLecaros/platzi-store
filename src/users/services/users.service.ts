bc7195bb8d2f6c2b6af7791e4bee41f12519a9d7
Matias <mtorolecaros@gmail.com>
2023-03-16 19:46:09 -0300

    clase 9

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { User } from '../entities/user.entity';
 import { Order } from '../entities/order.enity';
@@ -10,7 +11,7 @@ import { ProductsService } from './../../products/services/products.service';
 export class UsersService {
   constructor(
     private productsService: ProductsService,
-    @Inject('API_KEY') private apikey: string,
+    private configService: ConfigService,
   ) {}
 
   private counterId = 1;
@@ -24,6 +25,9 @@ export class UsersService {
   ];
 
   findAll() {
+    const apikey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    console.log(apikey, dbName);
     return this.users;
   }
 
