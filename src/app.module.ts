14daaf0c222c5ebbeaf5c87b295845b28d7496b8
Matias <mtorolecaros@gmail.com>
2023-03-17 21:07:12 -0300

    Clase 8 TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-
 import { enviroments } from './enviroments';
 import config from './config';
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { resolve } from 'path';
+import { Client } from 'pg';
+
 import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apikey: string,
+    @Inject('PG') private clientPG: Client,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
@@ -13,5 +17,16 @@ export class AppService {
     const apikey = this.configService.apikey;
     const name = this.configService.database.name;
     return `Hello World! ${apikey} ${name}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.clientPG.query('SELECT * FROM tasks', (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    });
   }
 }
