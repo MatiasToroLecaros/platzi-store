b1bf78f16221c25df4664920b6e8164d3929bf4d
Matias <mtorolecaros@gmail.com>
2023-03-20 13:45:37 -0300

    Clase 11 auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
-  imports: [UsersModule, PassportModule, LocalStrategy],
+  imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
