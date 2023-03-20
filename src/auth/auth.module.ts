bb927041c60d37a08082f6401e4d0be842ff249b
Matias <mtorolecaros@gmail.com>
2023-03-19 21:30:22 -0300

    Clase 9 auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [UsersModule, PassportModule, LocalStrategy],
+  providers: [AuthService, LocalStrategy],
+})
 export class AuthModule {}
