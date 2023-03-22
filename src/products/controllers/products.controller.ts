b64226dbc12227046951e1fcaf680f70556610fd
Matias <mtorolecaros@gmail.com>
2023-03-21 21:22:53 -0300

    clase 14 auth

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,13 +9,13 @@ import {
   Delete,
   HttpStatus,
   HttpCode,
-  Res,
+  // Res,
   UseGuards,
   // ParseIntPipe,
 } from '@nestjs/common';
-import { Response } from 'express';
+// import { Response } from 'express';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
 
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import {
@@ -25,13 +25,16 @@ import {
 } from '../dtos/products.dto';
 
 import { ProductsService } from './../services/products.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { Public } from '../../auth/decorators/public.decorator';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
   @Get()
   @ApiOperation({ summary: 'List of products' })
   getProducts(@Query() params: FilterProductsDto) {
@@ -43,6 +46,7 @@ export class ProductsController {
     return `yo soy un filter`;
   }
 
+  @Public()
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', ParseIntPipe) productId: number) {
